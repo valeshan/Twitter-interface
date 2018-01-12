8c8c3160973512ae09d1b11f063d21d84fbe94c3
valeshan <valeshan@gmail.com>
2018-01-13 00:18:06 +1100

    -added modules
    -added body and cookie parser, nodemon and pug.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.user('/static', express.static('public'));
+
+app.set('view engine', 'pug');
